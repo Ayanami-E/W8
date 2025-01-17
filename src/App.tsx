75885fe99c1846f5ca822e77d399db41df1a369b
Ayanami-E <150331674+Ayanami-E@users.noreply.github.com>
2025-01-17 23:20:23 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,20 +2,14 @@ import React from "react";
 import MyContainer from "./components/MyContainer";
 import "./App.css"; // 确保 App.css 存在并包含一些基础样式
 
-const App: React.FC = () => {
+function App() {
   return (
     <div className="app-container">
-      <header>
-        <h1>Welcome to My React App</h1>
-      </header>
-      <main>
-        <MyContainer />
-      </main>
-      <footer>
-        <p>© 2025 My React App. All rights reserved.</p>
-      </footer>
+      <h1>Hello World!</h1>
+      <MyContainer />
     </div>
   );
-};
+}
+
 
 export default App;
