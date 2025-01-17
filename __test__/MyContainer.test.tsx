879f39876c9de0a8396e35f2f108c8f1035b1bc5
Ayanami-E <150331674+Ayanami-E@users.noreply.github.com>
2025-01-18 00:37:04 +0200

    Delete __test__ directory

--- __test__/MyContainer.test.tsx
+++
@@ -1,8 +0,0 @@
-import '@testing-library/jest-dom';
-import { render, screen } from "@testing-library/react";
-import MyContainer from "../src/components/MyContainer";
-
-test("renders MyContainer component", () => {
-  render(<MyContainer />);
-  expect(screen.getByText("Welcome to MyContainer")).toBeInTheDocument();
-});
