929e94ff3928c80ce371c186183b1a019b45409a
Ayanami-E <150331674+Ayanami-E@users.noreply.github.com>
2025-01-18 09:08:24 +0200

    Delete jest.config.cjs

--- jest.config.cjs
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  preset: "ts-jest",
-  testEnvironment: "jsdom",
-  testResultsProcessor: "jest-junit", // 让 Jest 生成 junit.xml
-  moduleNameMapper: {
-    "\\.(css|less|sass|scss)$": "identity-obj-proxy" // 让 Jest 忽略 CSS
-  }
-};
