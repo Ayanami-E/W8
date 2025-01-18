e316c5b2373006c66284137bc5ae375dea4539fd
Ayanami-E <150331674+Ayanami-E@users.noreply.github.com>
2025-01-18 09:27:50 +0200

    Update jest.config.js

--- jest.config.js
+++ jest.config.js
@@ -5,7 +5,6 @@ module.exports = {
     '\\.(css|less|sass|scss)$': 'identity-obj-proxy',
   },
   setupFilesAfterEnv: ['@testing-library/jest-dom'],
-  testMatch: ['<rootDir>/src/__test__/**/*.test.{ts,tsx}'],  // 添加这行
   transform: {
     '^.+\\.tsx?$': ['ts-jest', {
       tsconfig: 'tsconfig.app.json'
@@ -16,10 +15,6 @@ module.exports = {
     ['jest-junit', {
       outputDirectory: 'test-results',
       outputName: 'junit.xml',
-      suiteName: "jest tests",
-      classNameTemplate: "{classname}",
-      titleTemplate: "{title}",
-      ancestorSeparator: " › "
     }]
   ]
 };
