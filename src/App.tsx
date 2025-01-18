278c3d397877c1d3b4b8ead3c1c53de3a0cc2ed1
Ayanami-E <150331674+Ayanami-E@users.noreply.github.com>
2025-01-18 09:58:56 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
+import React from "react";
+import MyContainer from "./MyContainer";
+
 function App() {
   return (
     <div className="App">
       <h1>Hello World!</h1>
+      <MyContainer />
     </div>
   );
 }
